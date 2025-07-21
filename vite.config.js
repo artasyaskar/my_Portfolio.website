87470eab0b26fdc1026638717146220efe06c321
Artas Yaskar <artasyaskar@gmail.com>
2025-07-21 11:30:46 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
+// vite.config.js
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  root: '.', // ensure root is current directory
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       input: {
-        main: 'src/index.html',
-        projects: 'src/projects.html'
-      },
-    },
+        main: resolve(__dirname, 'index.html'),        // root/index.html
+        projects: resolve(__dirname, 'projects.html')  // root/projects.html
+      }
+    }
   },
   server: {
-    port: 3000,
-  },
+    port: 3000
+  }
 });
